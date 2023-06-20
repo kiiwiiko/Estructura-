6d487380d68626cc6462c46ce6aa3a0407405788
Arie Quimi <trr.2002j@gmail.com>
2023-06-20 12:56:30 -0500

    Estructura principal del programa

---
+++ ejercicio.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+struct persona
+    {
+        char nombre[25];
+        char direccion[50];
+        char carrera[25];
+        int edad;
+        float promedio;
+    }alumno[3];
+
